65a72300471cd62846975e411f25bde68d3a7394
franklyn77 <elprincipee77@gmail.com>
2018-10-11 23:39:39 -0400

    Close Window using MVVM pattern in WPF

---
+++ CloseWindowWPFApp/CloseWindowWPFApp/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CloseWindowWPFApp
+{
+    public class MainViewModel
+    {
+        //Properties
+        public RelayCommand<IClosable> CloseWindowCommand { get; private set; }
+
+        //Constructor
+        public MainViewModel()
+        {
+            this.CloseWindowCommand = new RelayCommand<IClosable>(this.CloseWindow);
+        }
+
+        //Methods
+        private void CloseWindow(IClosable window)
+        {
+            if (window != null)
+                window.Close();
+        }
+    }
+}
